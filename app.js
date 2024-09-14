2408541828cc58cc66bba279615c045168e12392
ChloeIR <chloerodgers79@gmail.com>
2024-09-14 22:49:35 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    var countDownDate = new Date("Oct 31, 2024 00:00:00").getTime();
+
+    var x = setInterval(function() {
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
+        var days = Math.floor(distance/(1000*60*60*24));
+        var hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+        var minutes = Math.floor((distance%(1000*60*60))/(1000*60));
+        var seconds = Math.floor((distance%(1000*60))/1000);
+
+        document.getElementById("Countdown_to_Halloween").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("Countdown_to_Halloween").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+})
